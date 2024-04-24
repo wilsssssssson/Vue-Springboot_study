c05e0157a7ee753246fb2ff8c79b584acf6a216c
wilsssssssson <1327003704@qq.com>
2024-04-24 17:35:56 +0800

    2024/4/24 hjx first commit

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <div>{{ message }}</div>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message:"我是你爹"
+    }
+  },
+}
+</script>
