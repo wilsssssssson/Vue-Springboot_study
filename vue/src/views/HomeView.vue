37f2fab1c115c5646b5c24f5aa8365ea713619d8
wilsssssssson <1327003704@qq.com>
2024-04-25 21:34:27 +0800

    2024/4/5 hjx 前端框架

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <el-container style="height: 500px; border: 1px solid #eee">
-  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-    <el-menu :default-openeds="['1', '3']">
+  <div style="height: 100%">
+    <el-container style="height: 100%;">
+  <el-aside width="200px" style="background-color: rgb(238, 241, 246);height: 100%;">
+    <el-menu :default-openeds="['1', '3']" style="height: 100%;">
       <el-submenu index="1">
         <template slot="title"><i class="el-icon-message"></i>导航一</template>
         <el-menu-item-group>
@@ -52,7 +52,7 @@
   </el-aside>
   
   <el-container>
-    <el-header style="text-align: right; font-size: 12px">
+    <el-header>
       <el-dropdown>
         <i class="el-icon-setting" style="margin-right: 15px"></i>
         <el-dropdown-menu slot="dropdown">
@@ -64,7 +64,7 @@
       <span>王小虎</span>
     </el-header>
     
-    <el-main>
+    <el-main style="height: 100%">
       <el-table :data="tableData">
         <el-table-column prop="date" label="日期" width="140">
         </el-table-column>
@@ -93,8 +93,17 @@ export default {
         address: '上海市普陀区金沙江路 1518 弄'
       };
       return{
-        tableData: Array(20).fill(item)
+        tableData: Array(10).fill(item)
       };
   }
 }
 </script>
+
+<style>
+.el-header{
+  text-align: right;
+  font-size: 12px ;
+  border-bottom: 1px solid #eee;
+  line-height: 60px;
+}
+</style>
