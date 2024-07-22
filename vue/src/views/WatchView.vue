4a4975de1f5d87c11167d86439657f4566e8e2b7
wilsssssssson <1327003704@qq.com>
2024-07-22 17:54:54 +0800

    hjx  add:添加vue3资料

---
+++ vue/src/views/WatchView.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="about">
+      <h1>This is an about page</h1>
+      <button @click="add_tempt">水温{{temp}}</button>
+      <button @click="add_height">水位{{height}}</button>
+
+    </div>
+  </template>
+
+
+<script setup>
+
+import { ref,watch, watchEffect } from 'vue';
+
+let temp=ref(0);
+let height=ref(0);
+
+
+function add_height(){
+    height.value+=1;
+}
+function add_tempt(){
+    temp.value+=1;
+}
+
+watch([temp,height],(val)=>{
+    let [tempval,heightval]=val;
+    if(tempval>10){
+        console.log("this is watch for temp");
+    }
+    if(heightval>0){
+        console.log("this is watch for height");
+    }
+}, { immediate: true, once: true })
+
+
+watchEffect(()=>{
+    if(temp.value>50){
+        console.log("this is watcheffect for temp");
+    }
+    if(height.value>30){
+        console.log("this is watcheffect for height");
+    }
+})
+
+</script>
